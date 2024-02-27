15162c30838bc29d4d0238e9098e370db2dc46c4
Rodrigo Teixeira <71793817+ichiruto@users.noreply.github.com>
2024-02-27 17:33:33 -0300

    gerar relatorios feito

--- front/src/components/Relatorios/relatorios.vue
+++ front/src/components/Relatorios/relatorios.vue
@@ -1,80 +1,79 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <AppBar />
-  <v-app>
-    <v-container>
-      <v-autocomplete
-        label="Selecione o Relatório"
-        :items="relatoriosLocalStorage"
-        item-title="codigo"
-        item-value="codigo"
-        v-model="selectedRelatorio"
-        @keyup.enter="montarPainelFiltros"
-        variant="outlined"
-      ></v-autocomplete>
-      <v-row v-if="parametrosArray.length > 0">
-        <h1>Seleções:</h1>
-        <v-col
-          v-for="(parametro, index) in parametrosArray"
-          :key="index"
-          cols="12"
-          md="6"
-        >
-          {{ parametro.label }}
-          <v-text-field
-            v-if="parametro.tipo === 'DATE'"
-            v-model="parametro.parametro"
-            type="date"
-            outlined
-          ></v-text-field>
-          <v-text-field
-            v-mask="'####'"
-            v-if="parametro.tipo === 'INTEGER'"
-            v-model="parametro.parametro"
-            type="number"
-            outlined
-          ></v-text-field>
-          <v-text-field
-            v-mask="'###.##'"
-            v-if="parametro.tipo === 'NUMERIC'"
-            v-model="parametro.parametro"
-            type="number"
-            outlined
-          ></v-text-field>
-          <v-text-field
-            v-if="parametro.tipo === 'TEXT'"
-            v-model="parametro.parametro"
-            outlined
-          ></v-text-field>
+  <v-container>
+    <v-autocomplete
+      label="Selecione o Relatório"
+      :items="relatoriosLocalStorage"
+      item-title="codigo"
+      item-value="codigo"
+      v-model="selectedRelatorio"
+      :change="montarPainelFiltros()"
+      variant="outlined"
+    ></v-autocomplete>
 
-          <v-autocomplete
-            v-if="parametro.tipo === 'ARRAY'"
-            :items="JSON.parse(parametro.dominio)"
-            item-title="chave"
-            item-value="valor"
-            v-model="parametro.parametro"
-            outlined
-          ></v-autocomplete>
-          <v-autocomplete
-            v-if="
-              parametro.tipo === 'TABLE' &&
-              comboSelecao[index]?.tipo === 'TABLE'
-            "
-            :key="index"
-            :items="comboSelecao[index]?.resultado[0].func_json_consulta"
-            label="Selecione o item"
-            item-title="chave"
-            item-value="valor"
-            v-model="parametro.parametro"
-            outlined
-          ></v-autocomplete>
-        </v-col>
-        <v-btn class="me-4" color="red" @click="gerarRelatorios">
-          Gerar Relatorio
-        </v-btn>
-      </v-row>
+    <v-container v-if="parametrosArray.length > 0">
+      <h1>Seleções:</h1>
+      <v-col
+        v-for="(parametro, index) in parametrosArray"
+        :key="index"
+        cols="12"
+        md="6"
+      >
+        {{ parametro.label }}
+
+        <v-text-field
+          v-if="parametro.tipo === 'DATE'"
+          v-model="parametro.valor"
+          type="date"
+          outlined
+        ></v-text-field>
+        <v-text-field
+          v-mask="'####'"
+          v-if="parametro.tipo === 'INTEGER'"
+          v-model="parametro.parametro"
+          type="number"
+          outlined
+        ></v-text-field>
+        <v-text-field
+          v-mask="'###.##'"
+          v-if="parametro.tipo === 'NUMERIC'"
+          v-model="parametro.parametro"
+          type="number"
+          outlined
+        ></v-text-field>
+        <v-text-field
+          v-if="parametro.tipo === 'TEXT'"
+          v-model="parametro.parametro"
+          outlined
+        ></v-text-field>
+
+        <v-autocomplete
+          v-if="parametro.tipo === 'ARRAY'"
+          :items="JSON.parse(parametro.dominio)"
+          item-title="chave"
+          item-value="valor"
+          v-model="parametro.parametro"
+          outlined
+        ></v-autocomplete>
+        <v-autocomplete
+          v-if="
+            parametro.tipo === 'TABLE' && comboSelecao[index]?.tipo === 'TABLE'
+          "
+          :key="index"
+          :items="comboSelecao[index]?.resultado[0].func_json_consulta"
+          label="Selecione o item"
+          item-title="chave"
+          item-value="valor"
+          v-model="parametro.valor"
+          outlined
+        ></v-autocomplete>
+      </v-col>
+      <v-btn class="me-4" color="red" @click="gerarRelatorios">
+        Gerar Relatorio
+      </v-btn>
     </v-container>
-  </v-app>
+  </v-container>
 </template>
 
 <script setup>
@@ -87,6 +86,7 @@ export default {
     relatoriosLocalStorage: [],
     selectedRelatorio: null,
     parametrosArray: [],
+    relatorioLink: [],
     comboSelecao: [],
   }),
   created() {
@@ -106,6 +106,9 @@ export default {
       );
       if (relatorio) {
         this.parametrosArray = [...relatorio.parametros];
+        this.parametrosArray.forEach((parametro) => {
+          parametro.valor = "";
+        });
         const response = await axios.post(
           `${process.env.MANAGEMENT_API_URL}/relatoriosQuery`,
           relatorio
@@ -115,30 +118,22 @@ export default {
         this.parametrosArray = [];
       }
     },
-    gerarRelatorios() {
+
+    async gerarRelatorios() {
       const relatorio = this.relatoriosLocalStorage.find(
         (item) => item.codigo === this.selectedRelatorio
       );
-      const parametros = JSON.parse(JSON.stringify([...relatorio.parametros]));
-      console.log(parametros.parametro);
-
+      const parametros = JSON.parse(JSON.stringify(relatorio.parametros));
       parametros.forEach((parametro) => {
-        // Verificar se o nome do parâmetro é uma chave em parametros_url
-        if (
-          Object.prototype.hasOwnProperty.call(
-            relatorio.parametros_url,
-            parametro.parametro
-          )
-        ) {
-          console.log(parametro.valor);
-          relatorio.parametros_url[parametro.parametro] = parametro.valor;
-        }
+        relatorio.parametros_url[parametro.parametro] = parametro.valor;
       });
-
-      // Exibe o relatório gerado
       console.log(relatorio.parametros_url);
-
-      // Exibe o relatório gerado
+      const response = await axios.post(
+        `${process.env.MANAGEMENT_API_URL}/gerarRelatorios`,
+        relatorio.parametros_url
+      );
+      this.relatorioLink = response.data;
+      window.open(this.relatorioLink, "_blank");
     },
   },
 };
