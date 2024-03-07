cb648ab13801a80094e9e17d227bcc08c27be247
Rodrigo Teixeira <71793817+ichiruto@users.noreply.github.com>
2024-03-07 15:15:21 -0300

    gerar titulos front feito

--- src/components/GerarTitulos/GerarTitulosHome.vue
+++ src/components/GerarTitulos/GerarTitulosHome.vue
@@ -4,43 +4,70 @@ import AppBar from "@/layouts/default/AppBar.vue";
 
 <template>
   <AppBar />
-  <v-autocomplete
-    v-model="test"
-    :items="lotesCombo"
-    label="Selecione um lote"
-    item-title="nome"
-    item-value="id"
-  ></v-autocomplete>
-  <div class="btn-pointer" @click="redirectToView(item.id)">
-    <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
-  </div>
+  <v-row class="mt-6" no-gutters>
+    <v-col class="ml-2">
+      <v-autocomplete
+        v-model="selectedItem"
+        :items="lotesCombo"
+        label="Selecione um lote"
+        item-title="nome"
+        item-value="token"
+      ></v-autocomplete>
+    </v-col>
+    <div>
+      <v-col class="btn-pointer" @click="gerarReceitas(selectedItem)">
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" size="lg" />
+      </v-col>
+    </div>
+    <v-col>
+      <v-text-field
+        class="ml-6"
+        v-model="lotes.data_vencimento"
+        type="date"
+        label="Data de vencimento"
+      ></v-text-field>
+    </v-col>
+    <v-col class="ml-5">
+      <v-text-field
+        v-model="lotes.valor"
+        type="number"
+        label="Valor total"
+        readonly
+        disabled
+        class="opacity-75"
+      ></v-text-field>
+    </v-col>
+    <div>
+      <v-col
+        class="btn-pointer"
+        @click="gerarBoletos()"
+        :class="{ disabled: valorTotal <= 0 }"
+      >
+        <font-awesome-icon :icon="['fas', 'plus']" size="lg" />
+      </v-col>
+    </div>
+  </v-row>
+
   <v-text-field
-    v-model="test"
-    type="date"
-    label="Data de vencimento"
+    v-model="lotes.observacao"
+    type="text"
+    label="Observação"
   ></v-text-field>
-  <v-text-field v-model="test" type="number" label="Valor total"></v-text-field>
-  <div class="btn-pointer" @click="redirectToRegister()">
-    <font-awesome-icon :icon="['fas', 'plus']" />
-  </div>
-  <v-text-field v-model="test" type="text" label="Observação"></v-text-field>
+
   <v-data-table
     :headers="headers"
     :search="searchQuery"
-    :items="lotes"
+    :items="receitas"
+    v-model="selected"
     :rows-per-page-items="itemsPerPage"
     :footer-props="footerProps"
+    show-select
     density="default"
+    item-selectable="selectable"
+    @change="sumCheckedValues"
   >
-    <!-- eslint-disable vue/valid-v-slot -->
-    <template v-slot:item.actions="{ item, index }">
-      <div class="custom-td">
-        <v-checkbox
-          v-model="lotesChecked[index]"
-          @change="sumCheckedValues"
-        ></v-checkbox>
-      </div>
-    </template>
+    <!-- eslint-disable-next-line vue/valid-v-slot -->
+    <template v-slot:item.header> </template>
   </v-data-table>
 </template>
 <script>
@@ -52,10 +79,19 @@ export default {
   },
   data() {
     return {
+      selected: [],
+      selectedItem: null,
       lotesChecked: [],
-      lotes: [],
+      lotes: {
+        data_vencimento: null,
+        valor: null,
+        observacao: null,
+      },
+
+      receitas: [],
       lotesCombo: [],
-      test: [],
+      boleto: [],
+      testVal: [],
       searchQuery: "",
       itemsPerPage: [20],
       footerProps: [20],
@@ -78,36 +114,67 @@ export default {
         },
         {
           title: "Observação",
-          value: "observaao",
-        },
-        {
-          value: "actions",
+          value: "observacao",
         },
       ],
     };
   },
   computed: {
-    filteredLotes() {
-      const query = this.searchQuery.toLowerCase().trim();
-
-      const filteredItems = this.lotes.filter((item) => {
-        const descricao = item.lote.toLowerCase();
-        const prediosAreasDescricao = item.predios_areas
-          ? item.predios_areas.descricao.toLowerCase()
-          : "";
-
-        return (
-          descricao.includes(query) || prediosAreasDescricao.includes(query)
-        );
-      });
-
-      // Ordena os itens pelo nome da descrição
-      return filteredItems.sort((a, b) =>
-        a.descricao.localeCompare(b.descricao)
-      );
+    valorTotal() {
+      return this.lotes.valor || 0;
     },
   },
   methods: {
+    async gerarReceitas(selectedItem) {
+      try {
+        const selectedToken = selectedItem;
+        console.log(selectedToken);
+        const data = {
+          lote_token: selectedToken,
+        };
+        const response = await axios.post(
+          `${process.env.MANAGEMENT_API_URL}/loteReceita`,
+          data
+        );
+        const responseData = response.data[0].func_json_receitas_lote;
+        this.receitas = responseData;
+        console.log(this.receitas);
+      } catch (error) {
+        console.error("Erro ao carregar receitas:", error);
+      }
+    },
+
+    async gerarBoletos() {
+      try {
+        const storedTokenPredio = JSON.parse(localStorage.getItem("predio"));
+        const storedTokenUser = JSON.parse(localStorage.getItem("user"));
+        const selectedToken = this.selectedItem;
+        const receitasSelecionadas = this.selected.map((itemId) => {
+          const receita = this.receitas.find((item) => item.id === itemId);
+          return { receita_token: receita.token };
+        });
+        const data = {
+          observacao: this.lotes.observacao,
+          valor: this.lotes.valor,
+          data_vencimento: this.lotes.data_vencimento,
+          lote_token: selectedToken,
+          predio_token: storedTokenPredio.predio_token,
+          user_token: storedTokenUser.token,
+          receitas: receitasSelecionadas,
+        };
+        console.log(data);
+        const response = await axios.post(
+          `${process.env.MANAGEMENT_API_URL}/geraBoletos`,
+          data
+        );
+        const responseData = response.data[0].func_gera_boleto_lote;
+        this.boleto = responseData;
+        console.log(this.boleto);
+      } catch (error) {
+        console.error("Erro ao carregar receitas:", error);
+      }
+    },
+
     async loadLotes() {
       const storedToken = JSON.parse(localStorage.getItem("predio"));
       const data = {
@@ -120,7 +187,6 @@ export default {
         );
         const responseData = response.data[0].func_json_lotes_predio;
         this.lotesCombo = responseData;
-        console.log(this.lotesCombo);
       } catch (error) {
         console.error("Erro ao carregar áreas:", error);
       }
@@ -133,33 +199,17 @@ export default {
       console.log("saveSearchQuery:", searchQuery); // Imprime no console
     },
 
-    // sumCheckedValues() {
-    //   // Filtra os itens marcados
-    //   const checkedItems = this.lotes.filter(
-    //     (item, index) => this.lotesChecked[index]
-    //   );
-    //   // Calcula a soma dos valores
-    //   if (item.cobrar === "COBRAR") {
-    //     console.log("##################################\n", item.cobrar);
-    //   }
-    //   const sum = checkedItems.reduce(
-    //     (total, item) => total + parseFloat(item.valor),
-    //     0
-    //   );
-    //   // Exibe o resultado
-    //   alert(A soma dos valores das checkboxes marcadas é: ${sum});
-    // },
     sumCheckedValues() {
       // Filtra os itens marcados
-      const checkedItems = this.lotes.filter(
-        (item, index) => this.lotesChecked[index]
+      const selectedItems = this.receitas.filter((item) =>
+        this.selected.includes(item.id)
       );
 
       // Inicializa a soma
       let sum = 0;
 
       // Itera sobre os itens marcados
-      checkedItems.forEach((item) => {
+      selectedItems.forEach((item) => {
         // Verifica se o item deve ser cobrado
         if (item.cobrar === "COBRAR") {
           // Adiciona o valor apenas se a condição for atendida
@@ -175,25 +225,11 @@ export default {
           }
         }
       });
+      this.lotes.valor = sum; // Atualiza o valor do campo lotes.valor
     },
   },
   mounted() {
     this.loadLotes();
-    const storedToken = JSON.parse(localStorage.getItem("predio"));
-    const data = {
-      predio_id: storedToken.predio_id,
-    };
-    axios
-      .post(`${process.env.MANAGEMENT_API_URL}/listaLotesReceita`, data)
-      .then((response) => {
-        this.lotes = response.data;
-      })
-      .catch((error) => {
-        console.error("Erro na chamada de API:", error);
-      });
-    // Recarrega o valor do campo de pesquisa do localStorage
-    const searchQuery = localStorage.getItem("searchQuery");
-    this.searchQuery = searchQuery || "";
   },
 };
 </script>
@@ -215,6 +251,7 @@ export default {
 }
 
 .btn-pointer {
+  margin-left: 20px;
   cursor: pointer;
 }
 
@@ -224,5 +261,10 @@ export default {
 
 .gray-icon {
   color: gray;
+}
+
+.disabled {
+  pointer-events: none;
+  opacity: 0.5;
 }
 </style>
